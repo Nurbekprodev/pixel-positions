89427710f291b0b02ccffda6568267d34cd13075
Makhmadaminov Nurbek <nurbekprodev@gmail.com>
2026-04-13 07:44:36 +0900

    updated

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,17 +1,17 @@
+@props(['job'])
+
 <x-panel class="flex-col text-center">
-    <div class="self-start text-sm">Laracasts</div>
+    <div class="self-start text-sm">{{ $job->employer->name }}</div>
     <div class="py-8 ">
-        <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-300 ">Video Producer</h3>
+        <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-300 ">{{ $job->title }}</h3>
         
-        <p class="mt-4 text-sm">Full Time - From $60,000</p>
+        <p class="mt-4 text-sm">{{$job->schedule}} - From {{$job->salary}}</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <x-tag size="small">Frontend</x-tag>
-            <x-tag size="small">Backend
-                
-            </x-tag>
-            <x-tag size="small">Manager</x-tag>
+            @foreach($job->tags as $tag)
+                <x-tag :tag="$tag" size="small" />
+            @endforeach
         </div>
         <div>
             <x-employer-logo :width="42" />
--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,4 +1,4 @@
-@props(['size' => 'base'])
+@props(['tag', 'size' => 'base'])
 
 @php
     $classes = 'bg-white/10 rounded-xl  font-bold hover:bg-white/25 transition';
@@ -12,4 +12,4 @@
     }
 @endphp
 
-<a class="{{$classes}}" href="#">{{ $slot }}</a>
+<a class="{{$classes}}" href="/tags/{{ strtolower($tag->name) }}">{{ $tag->name }}</a>
